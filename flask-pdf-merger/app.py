3ded783f35473170b8874553b9e07bbb41c39baf
Arun Thakur <aruneshsinghrajawat@gmail.com>
2025-09-22 00:40:40 +0530

    Update app.run configuration to use environment variable for port

--- flask-pdf-merger/app.py
+++ flask-pdf-merger/app.py
@@ -536,4 +536,6 @@ def clear_session():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000,host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
